f3a21a546721dbe68a764bdcc455c3e944fca240
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2025-11-14 16:50:52 -0500

    Ejercicio Examen Mios

--- Problema2.scala
+++ Problema2.scala
@@ -2,7 +2,7 @@ class Problema2 {
   def ajustedePrecios(precios: List[Double], politica: Double => Double): List[Double] = {
     precios.map(politica)
   }
-  val ImpuestoIva: Double => Double = precio => precio * 1.12
+  val ImpuestoIva: Double =  precio * 1.12
   val Descuento: Double => Double = precio => precio * 0.80
   val Aumento: Double => Double = precio => precio + 5
 }
